d175abc8b682922d9544c08f90270183e8bea000
Doomsdhg <tohanknv@yandex.ru>
2022-08-28 19:48:09 +0300

    SRVR-07: synchronize option set back to false

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -8,6 +8,6 @@ export const ormConfig: TypeOrmModuleOptions = {
       rejectUnauthorized: false,
     },
     entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true,
+    synchronize: false,
     autoLoadEntities: true,
   };
