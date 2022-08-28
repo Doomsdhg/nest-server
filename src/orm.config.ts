2624e9c7ddced4cdc746231b1d89f3ac43442ede
Doomsdhg <80508769+Doomsdhg@users.noreply.github.com>
2022-08-28 19:56:07 +0300

    SRVR-07: heroku deploy fixes implemented, deploy attempt (#8)
    * SRVR-07: heroku deploy fixes implemented

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const ormConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    database: 'nest_api',
-    username: 'anton',
-    password : 'qwe123',
-    host: 'localhost',
+    url: process.env.DATABASE_URL,
     port: 5432,
+    type: 'postgres',
+    ssl: {
+      rejectUnauthorized: false,
+    },
     entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: false
-};
+    synchronize: false,
+    autoLoadEntities: true,
+  };
