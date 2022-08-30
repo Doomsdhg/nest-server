be0f3d1824e536f3b124c58026c4f2715a85c8c2
Doomsdhg <tohanknv@yandex.ru>
2022-08-30 14:15:31 +0300

    SRVR-08: cors disabled

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { Constants } from './constants/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   await app.listen(process.env.PORT || Constants.GENERAL.DEFAULT_PORT);
 }
 bootstrap();
